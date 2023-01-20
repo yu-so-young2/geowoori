d881de2a7e609d1392866294d48377454b1ef130
soyoung <sy980911@gmail.com>
2023-01-20 15:05:06 +0900

    add : test spring-boot project 추가

---
+++ test/test/src/main/java/com/ssafy/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		System.out.printf("asaddsa");
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
