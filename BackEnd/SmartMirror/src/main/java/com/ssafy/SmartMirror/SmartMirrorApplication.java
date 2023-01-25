ddbc4ac20367fcc07768514d475bd345ba14e931
soyoung <sy980911@gmail.com>
2023-01-25 14:46:36 +0900

    add : BackEnd folder 생성

---
+++ BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/SmartMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.SmartMirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMirrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMirrorApplication.class, args);
+	}
+
+}
