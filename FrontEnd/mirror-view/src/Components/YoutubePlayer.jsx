60add8ed201214713d9e999e95021d3214a9e835
nohhyunjeong93 <nohhyunjeong93@mgmail.com>
2023-01-26 17:35:17 +0900

    feat : youtube api 연결

--- FrontEnd/mirror-view/src/Components/YoutubePlayer.jsx
+++ FrontEnd/mirror-view/src/Components/YoutubePlayer.jsx
@@ -17,19 +17,23 @@ function YoutubePlayer(){
             const list = res?.data?.items;
             const new_list = list.map((x) => x?.snippet.resourceId.videoId); 
             setPlayList([new_list])
-            console.log(new_list)
         }).catch((err) => {
             console.log(err)
         })
         
     }, [])
     
-
+    const videoList = playList.map((id) => {
+        
+        return (
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        )
+    })
     
     return (
         <React.Fragment>
             <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/UExwaDJ4Y1QyQ0pBSXpmX09rY1ZZanc4cWh4N3d5eEVfRC41NkI0NEY2RDEwNTU3Q0M2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/UExwaDJ4Y1QyQ0pBSXpmX09rY1ZZanc4cWh4N3d5eEVfRC41NkI0NEY2RDEwNTU3Q0M2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </React.Fragment>
     )
