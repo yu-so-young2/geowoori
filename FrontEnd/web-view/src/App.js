5fd5017d49032147d176f5e0d878f630c54287cd
원영현 <hello6815@naver.com>
2023-01-27 10:17:14 +0900

    add : SignUp.jsx

--- FrontEnd/web-view/src/App.js
+++ FrontEnd/web-view/src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import { Login, FindUser, NotFound404 } from './Pages';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import logo from "./logo.svg";
+import { Login, FindUser, NotFound404 } from "./Pages";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />}/>    
-          {/* <Route path="/signup" element={}/>     */}
-          <Route path="/find-user" element={<FindUser />}/>
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/find-user" element={<FindUser />} />
+        <Route path="*" element={<NotFound404 />} />
+      </Routes>
+    </div>
   );
 }
 
