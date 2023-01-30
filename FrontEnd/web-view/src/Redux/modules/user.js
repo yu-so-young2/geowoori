9b66d305e629707a1aa5bdc84f77059153090776
원영현 <hello6815@naver.com>
2023-01-30 10:49:23 +0900

    test : merge

--- FrontEnd/web-view/src/Redux/modules/user.js
+++ FrontEnd/web-view/src/Redux/modules/user.js
@@ -2,9 +2,11 @@ import { createAction, handleActions } from "redux-actions";
 
 //Action TYPE
 const LOGIN = "user/LOGIN";
+const REGISTER_USER = "user/REGISTER_USER";
 
 // Action creator
 const login = createAction(LOGIN);
+const registerUser = createAction(REGISTER_USER);
 
 //InitialState
 const initialState = {
@@ -13,6 +15,7 @@ const initialState = {
 
 const userReducer = handleActions({
   [LOGIN]: (state, action) => ({ user: state.user }),
+  [REGISTER_USER]: (state, action) => ({ user: state.user }),
 });
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
