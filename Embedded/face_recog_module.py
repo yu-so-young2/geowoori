4d2f4f57ca977dab518c1c7f21f2f1114406b3ef
서보민 <sbmsky@gmail.com>
2023-01-30 16:45:05 +0900

    feat : EB backend main code

---
+++ Embedded/face_recog_module.py
@@ -0,0 +1,81 @@
+import face_recognition
+import cv2
+import os
+import numpy as np
+
+
+class FaceRecog():
+    def __init__(self):
+        self.camera = cv2.VideoCapture(0)
+        self.known_face_encodings = []
+        self.known_face_names = []
+
+        # Load sample pictures and learn how to recognize it.
+        dirname = 'knowns'
+        files = os.listdir(dirname)
+        for filename in files:
+            name, ext = os.path.splitext(filename)
+            if ext == '.jpg':
+                self.known_face_names.append(name)
+                pathname = os.path.join(dirname, filename)
+                img = face_recognition.load_image_file(pathname)
+                face_encoding = face_recognition.face_encodings(img)[0]
+                self.known_face_encodings.append(face_encoding)
+
+        # Initialize some variables
+        self.found_face = False
+        self.face_locations = []
+        self.face_encodings = []
+        self.face_name = ""
+        self.process_this_frame = True
+
+    def __del__(self):
+        del self.camera
+
+    def destroycamera(self):
+        self.camera.release()
+
+    def get_frame(self):
+        # Grab a single frame of video
+        ret, frame = self.camera.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if self.process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            self.face_locations = face_recognition.face_locations(rgb_small_frame)
+            self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+            for face_encoding in self.face_encodings:
+                # See if the face is a match for the known face(s)
+                distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                min_value = min(distances)
+
+                # tolerance: How much distance between faces to consider it a match. Lower is more strict.
+                # 0.6 is typical best performance.
+                name = "Unknown"
+                if min_value < 0.6:
+                    index = np.argmin(distances)
+                    name = self.known_face_names[index]
+                    if self.face_name == name and self.face_name != "":
+                        self.found_face = True
+                    self.face_name = name
+
+        self.process_this_frame = not self.process_this_frame
+
+        return frame
+
+def face_recognitiontest():
+    face_recog = FaceRecog()
+    while not face_recog.found_face:
+        face_recog.get_frame()
+        cv2.waitKey(10)
+
+    face_recog.destroycamera()
+    cv2.destroyAllWindows()
+    return face_recog.face_name
--- Embedded/main.py
+++ Embedded/main.py
@@ -1,68 +1,90 @@
 import serial
 import time
-import os
+import threading
 import requests
-import sys
+import face_recog_module
+import asyncio
+import websockets
+
+class SmartMirror:
+    persondetected = False
+    detect_cnt = 0
+    def ultrasonic_arduino(self):
+        py_serial = serial.Serial(
+            port='COM5',
+            baudrate=9600,
+        )
+
+        while True:
+
+            time.sleep(0.1)
+            if py_serial.readable():
+                # 들어온 값이 있으면 값을 한 줄 읽음 (BYTE 단위로 받은 상태)
+                # BYTE 단위로 받은 response 모습 : b'\xec\x97\x86\xec\x9d\x8c\r\n'
+                response = py_serial.readline()
+
+                # 디코딩 후, 출력 (가장 끝의 \n을 없애주기위해 슬라이싱 사용)
+                distance = float(response[:len(response) - 1].decode())
+
+                if self.persondetected is False:
+                    if distance < 15:
+                        self.detect_cnt = self.detect_cnt + 1
+                else:
+                    if distance > 15:
+                        self.detect_cnt = self.detect_cnt + 1
+
+                if self.detect_cnt > 3:
+                    if self.persondetected is False:
+                        print(self.facerecogModule())
+                    self.persondetected = not self.persondetected
+                    self.detect_cnt = 0
 
 
+    def facerecogModule(self):
 
+        output = face_recog_module.face_recognitiontest()
+        datas = {'name': output}
+        url = "http://i8a201.p.ssafy.io:8080/test"
 
-def arduinotest():
-    py_serial = serial.Serial(
-        port='COM5',
-        baudrate=9600,
-    )
+        output = ""
+        try:
+            response = requests.post(url, data=datas)
+            output = response.text
+            print(output)
 
-    cnt = 0
+        except requests.exceptions.Timeout as errd:
+            print("Timeout Error : ", errd)
 
-    while True:
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting : ", errc)
 
-        time.sleep(0.1)
-        if py_serial.readable():
-            # 들어온 값이 있으면 값을 한 줄 읽음 (BYTE 단위로 받은 상태)
-            # BYTE 단위로 받은 response 모습 : b'\xec\x97\x86\xec\x9d\x8c\r\n'
-            response = py_serial.readline()
+        except requests.exceptions.HTTPError as errb:
+            print("Http Error : ", errb)
 
-            # 디코딩 후, 출력 (가장 끝의 \n을 없애주기위해 슬라이싱 사용)
-            distance = float(response[:len(response) - 1].decode())
-            if distance < 15:
-                cnt = cnt + 1
-            print(distance, cnt)
+        # Any Error except upper exception
+        except requests.exceptions.RequestException as erra:
+            print("AnyException : ", erra)
 
-            if cnt > 4 :
-                break;
+        return output
 
-def facerecog() :
-    print("face recog start")
-    stream = os.popen('python face_recog.py')
-    output = stream.read().strip()
+    def check_person_infrontofmirror(self):
+        while True:
+            if self.persondetected:
+                print("detected!",self.detect_cnt)
+            else:
+                print("not detected!",self.detect_cnt)
+            time.sleep(0.5)
 
-    datas = {'name' : output}
-    url = "http://i8a201.p.ssafy.io:8080/test"
-
-    output = ""
-    try:
-        response = requests.post(url, data=datas)
-        output = response.text
-
-    except requests.exceptions.Timeout as errd:
-        print("Timeout Error : ", errd)
-
-    except requests.exceptions.ConnectionError as errc:
-        print("Error Connecting : ", errc)
-
-    except requests.exceptions.HTTPError as errb:
-        print("Http Error : ", errb)
-
-    # Any Error except upper exception
-    except requests.exceptions.RequestException as erra:
-        print("AnyException : ", erra)
-
-    return output
+    def run(self):
+        x = threading.Thread(target=self.ultrasonic_arduino, daemon=True)
+        y = threading.Thread(target=self.check_person_infrontofmirror, daemon=True)
+        x.start()
+        y.start()
 
 
 if __name__ == "__main__":
+    sm = SmartMirror()
+    sm.run()
+
     while True:
-        arduinotest()
-        print(facerecog())
-    sys.exit()
+        time.sleep(0.1)
