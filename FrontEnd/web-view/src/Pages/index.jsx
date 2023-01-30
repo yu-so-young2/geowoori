ed5d250ea1b9864444434f7d7b857d37c313d32f
isabel-noh <nohhyunjeong93@gmail.com>
2023-01-30 14:27:35 +0900

    merge: develop merge

--- FrontEnd/web-view/src/Pages/index.jsx
+++ FrontEnd/web-view/src/Pages/index.jsx
@@ -1,7 +1,8 @@
 import Login from "./LogIn";
 import FindUser from "./FindUser";
+import SignUp from "./SignUp";
 import MemberPage from "./MemberPage";
 import AddMember from "./AddMember";
 import NotFound404 from './NotFound404';
 
-export { Login, FindUser, MemberPage, AddMember, NotFound404 }; 
+export { Login, FindUser, MemberPage, SignUp, AddMember, NotFound404 }; 
