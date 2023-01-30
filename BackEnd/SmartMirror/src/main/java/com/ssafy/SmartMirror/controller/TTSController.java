65a80bd48db59f7f6d0cd051a60a1b787a4fe413
soyoung <sy980911@gmail.com>
2023-01-30 14:29:19 +0900

    add : TTSController 추가

---
+++ BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/controller/TTSController.java
@@ -0,0 +1,11 @@
+package com.ssafy.SmartMirror.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class TTSController {
+
+
+}
