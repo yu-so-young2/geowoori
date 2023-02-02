aef68d11f3c33a00022f64a823ba4d53e9d38399
isabel-noh <nohhyunjeong93@gmail.com>
2023-02-02 09:12:35 +0900

    feat : kids 화면 전환

--- FrontEnd/mirror-view/src/App.js
+++ FrontEnd/mirror-view/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./App.css";
+import HomeHeader from "./Components/HomeHeader/HomeHeader";
 import BrushTeethVideo from "./Components/Kids/BrushTeethVideo";
+import Socket from "../Util/Socket";
 import Home from "./Pages/Home";
+import { Routes, Route } from 'react-router-dom'; 
+import Kids from "./Pages/Kids";
+import GeneralView from "./Pages/GeneralView";
 
 function App() {
   return (
     <React.Fragment>
-      <Home />
-      <BrushTeethVideo />
-      <Effect />
+      <Socket />
+      <HomeHeader />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/kids" element={<Kids />}/>
+        <Route path="/general" element={<GeneralView />}/>
+      </Routes>
     </React.Fragment>
   );
 }
--- FrontEnd/mirror-view/src/Pages/Home.jsx
+++ FrontEnd/mirror-view/src/Pages/Home.jsx
@@ -2,54 +2,17 @@ import React from "react";
 import HomeHeader from "../Components/HomeHeader/HomeHeader";
 import Kids from "./Kids";
 import GeneralView from "./GeneralView";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useSelector } from 'react-redux';
 
 function Home() {
-  //webSocket 통신
-  const webSocket = new WebSocket("ws://localhost:9998");
+  const isChild = useSelector(state => state?.member?.isChild);
+  const isAdult = useSelector(state => state?.member?.isAdult);
 
-  // server에서 보내주는 메시지
-  const [message, setMessage] = useState(null);
-
-  const [isChild, setIsChild] = useState(false);
-  const [isAdult, setIsAdult] = useState(false);
-
-  // 소켓이 열려있는 상태일 때 계속 진행되는 함수
-  webSocket.onopen = function () {
-    // console.log(webSocket)
-    // console.log('open')
-  };
-  webSocket.onclose = function () {
-    console.log("closed");
-  };
-  webSocket.onmessage = function (message) {
-    console.log(webSocket);
-    if (message.data === "1") {
-      // setIsChild(true)
-      // setIsAdult(false)
-      console.log(message.data);
-    } else {
-      console.log(message.data);
-      // setIsChild(false)
-      // setIsAdult(true)
-    }
-    const fileReader = new FileReader();
-    fileReader.onload = function (event) {
-      const arrayBuffer = event.target.result;
-      console.log(arrayBuffer);
-      // const dataview = new DataView(arrayBuffer);
-      // const answer = dataview.getFloat64(0);
-      // console.log(answer);
-    };
-    setMessage(message.data);
-  };
-  useEffect(() => {}, []);
 
   return (
     <React.Fragment>
-      <HomeHeader />
-      {isChild ? <Kids /> : null}
+      <Kids />
+      {/* {isChild ? <Kids /> : null} */}
       {isAdult ? <GeneralView /> : null}
     </React.Fragment>
   );
