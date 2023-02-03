085da3ee4666051109432ed9b36347a2e1537bf8
fftl <fftl0785@gmail.com>
2023-02-03 22:55:57 +0900

    feat : firebase file upload 1차 기능 생성

--- BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/controller/WebController.java
+++ BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/controller/WebController.java
@@ -1,12 +1,28 @@
 package com.ssafy.SmartMirror.controller;
 
+import com.google.firebase.auth.FirebaseAuthException;
+import com.ssafy.SmartMirror.config.FireBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/web")
 
 public class WebController {
+
+    @Autowired
+    private FireBaseService fireBaseService;
+
+    @PostMapping("/files")
+    public String uploadFile(@RequestParam("file") MultipartFile file,@RequestParam("name") String name) throws IOException, FirebaseAuthException {
+        if(file.isEmpty()){
+            return "is empty";
+        }
+
+        return fireBaseService.uploadFiles(file, name);
+    }
 }
