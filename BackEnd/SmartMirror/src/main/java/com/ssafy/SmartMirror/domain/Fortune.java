367057901e0f8a7d56b4a364e40e47ca8f08fbce
soyoung <sy980911@gmail.com>
2023-02-08 17:10:27 +0900

    edit : 기본key auto increment 추가

--- BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/domain/Fortune.java
+++ BackEnd/SmartMirror/src/main/java/com/ssafy/SmartMirror/domain/Fortune.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Builder
@@ -15,6 +17,7 @@ import javax.persistence.Id;
 @Entity
 public class Fortune { // 오늘의 한마디 (=포춘)
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long fortuneKey; // 기본키
     private String sentence; // 오늘의 한마디 문장
 }
