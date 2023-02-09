50b8ad7725c21b1c55041fb7ead7792823f5a9cf
원영현 <hello6815@naver.com>
2023-02-09 14:31:46 +0900

    add : random video

--- FrontEnd/mirror-view/src/App.js
+++ FrontEnd/mirror-view/src/App.js
@@ -7,24 +7,24 @@ import { Routes, Route } from "react-router-dom";
 import Kids from "./Pages/Kids";
 import GeneralView from "./Pages/GeneralView";
 import Slideshow from "./Elements/Slideshow";
+import BrushTeethVideo from "./Components/Kids/BrushTeethVideo";
 
 function App() {
   const webSocket = new WebSocket("ws://localhost:9998");
 
   return (
-    // <React.Fragment>
-    //   <Socket webSocket={webSocket}></Socket>
-    //   <HomeHeader />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/kids" element={<Kids webSocket={webSocket} />} />
-    //     <Route
-    //       path="/general"
-    //       element={<GeneralView webSocket={webSocket} />}
-    //     />
-    //   </Routes>
-    // </React.Fragment>
-    <Slideshow />
+    <React.Fragment>
+      <Socket webSocket={webSocket}></Socket>
+      <HomeHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kids" element={<Kids webSocket={webSocket} />} />
+        <Route
+          path="/general"
+          element={<GeneralView webSocket={webSocket} />}
+        />
+      </Routes>
+    </React.Fragment>
   );
 }
 
