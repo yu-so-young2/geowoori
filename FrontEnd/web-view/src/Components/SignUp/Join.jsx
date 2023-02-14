9ac880b298e45cd5789c48e364f482dc4e9ebb60
isabel-noh <nohhyunjeong93@gmail.com>
2023-02-14 10:35:20 +0900

    design: login, signup form and page design changed

--- FrontEnd/web-view/src/Components/SignUp/Join.jsx
+++ FrontEnd/web-view/src/Components/SignUp/Join.jsx
@@ -3,11 +3,18 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { actionCreators as userActions } from "../../Redux/modules/user";
 import { FormControl, Grid } from "@mui/material";
+import logo from '../../assets/logo.png';
 import "./Join.css";
+import { Button } from '@mui/material';
 
 function Join(props) {
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+
+  const goHome = (e) => {
+    e.preventDefault();
+    navigate('/');
+  }
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -52,7 +59,12 @@ function Join(props) {
         birthday: birthday,
         checked: checked,
       };
-
+      if (!checked){
+        window.alert('이용약관을 동의해주세요.');
+      }
+      else{
+        dispatch(userActions.registerUser(body));
+      }
       // dispatch(userActions.registerUser(body)).then((res) => {
       //   if (res.payload.success) {
       //     props.history.push("/login");
@@ -66,8 +78,79 @@ function Join(props) {
   );
 
   return (
-    <div>
-      <FormControl id="signup" variant="standard">
+    <>
+      <div className="signup-header">
+        <img id="logo" src={logo} onClick={goHome}></img>
+      </div>
+      <form id="signUp-form">
+        <div className="signup-form-div">
+          <div className="input-box">
+            <label htmlFor="email">이메일</label>
+            <div className="input-div">
+              <input 
+                id="email" name="email" type="email" 
+                value={email}
+                placeholder="username@gmail.com" required
+                onChange={onEmailHandler}/>
+            </div>
+          </div>
+          <div className="input-box">
+            <label htmlFor="password">비밀번호</label>
+            <div className="input-div">
+              <input 
+                id="password" name="password" type="password" 
+                value={password}
+                placeholder="비밀번호" required
+                onChange={onPasswordHandler}/>
+            </div>
+          </div>
+          <div className="input-box">
+            <label htmlFor="password_check">비밀번호 확인</label>
+            <div className="input-div">
+              <input 
+                id="password_check" name="password_check" type="password" 
+                value={confirmPassword}
+                placeholder="비밀번호 확인" required
+                onChange={onConfirmPasswordHandler}/>
+            </div>
+          </div>
+          <div className="input-box">
+            <label htmlFor="name">이름</label>
+            <div className="input-div">
+              <input 
+                id="name" name="name" type="text" 
+                value={name}
+                placeholder="이름" required
+                onChange={onNameHandler}/>
+            </div>
+          </div>
+          <div className="input-box">
+            <label htmlFor="birthday">생년월일</label>
+            <div className="input-div">
+              <input 
+                id="birthday" name="birthday" type="date" 
+                value={birthday} required
+                onChange={onBirthdayHandler}/>
+            </div>
+          </div>
+          <div className="term-div">
+            <p>이용약관, 개인정보 수집 및 이용,위치정보 이용약관(선택),<br/> 프로모션 안내 메일 수신(선택)에 모두 동의합니다.</p>
+          </div>
+        </div>
+        <div className="agree_div">
+          <input type="checkbox" onChange={onCheckedHandler}></input>
+          <label htmlFor="check">동의합니다</label>
+        </div>
+        <div className="login-btn-box">
+          {email.trim() != "" && password.trim() != "" && confirmPassword.trim() != "" && birthday.trim() != "" && name.trim() != "" ? 
+          <Button 
+          variant="contained"
+          onClick={onSubmitHandler}>회원가입</Button> :
+          <Button disabled>회원가입</Button>
+        }
+          </div>
+      </form>
+      {/* <FormControl id="signup" variant="standard">
         <Grid container spacing={1}>
           <Grid item xs={6}>
             <label htmlFor="email">Email</label>
@@ -138,8 +221,8 @@ function Join(props) {
             <button onClick={onSubmitHandler}>회원 가입</button>
           </Grid>
         </Grid>
-      </FormControl>
-    </div>
+      </FormControl> */}
+    </>
   );
 }
 
--- FrontEnd/web-view/src/Pages/Home.jsx
+++ FrontEnd/web-view/src/Pages/Home.jsx
@@ -6,15 +6,17 @@ import { HomeHeader, Profiles } from "../Components/index";
 
 function Home() {
   const navigate = useNavigate();
-  const is_login = sessionStorage.getItem('jwt');
+  const is_login = sessionStorage.getItem('jwt')? true : false;
+
   useEffect(() => {
+
     if (is_login) {
       return (
         <div className="container">
-          <HomeHeader />
-          <Profiles />
-          {/* <ProfileEdit /> */}
-        </div>
+        <HomeHeader />
+        <Profiles />
+        {/* <ProfileEdit /> */}
+      </div>
       )
     }
     else {
