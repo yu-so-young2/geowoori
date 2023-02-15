51589ada5a6f2e834229dd8fe3fdbb87e52b250b
isabel-noh <nohhyunjeong93@gmail.com>
2023-02-15 13:51:07 +0900

    fixed : photo fixed

--- FrontEnd/web-view/src/Components/Home/Profiles.jsx
+++ FrontEnd/web-view/src/Components/Home/Profiles.jsx
@@ -13,6 +13,8 @@ const api = axios.create({
 function Profiles() {
   const [memberList, setMemberList] = useState([]);
   // const userKey = localStorage.getItem('userKey');
+  const serialNumber = localStorage.getItem('serialNumber');
+  // const serialNumber = "8DLL-44yh-x7vB-VuWK";
   const userKey = "Fyw3-DOwW";
 
   useEffect(() => {
@@ -34,24 +36,27 @@ function Profiles() {
   };
   return (
     <>
-      <h1>멤버</h1>
-      {memberList.map((member) => {
-        return (
-          <React.Fragment 
-            key={member?.memberKey}>
-            <Profile
-              type="member" 
-              member={member}/>
-          </React.Fragment>
-        )
-      })}
-      <Profile type="add_member" />
-      <div className="footer-no-mirror">
-        <Button onClick={handleClick} variant="text">
-          아직 거울이 등록되지 않았습니다. <br /> 거울을 등록해주세요.
-        </Button>
-        <p></p>
+      <p className="profile-list-title">멤버</p>
+      <div className="profile-list">
+        {memberList.map((member) => {
+          return (
+            <React.Fragment 
+              key={member?.memberKey}>
+              <Profile
+                type="member" 
+                member={member}/>
+            </React.Fragment>
+          )
+        })}
+        <Profile type="add_member" />
       </div>
+      {!serialNumber &&      
+        <div className="footer-no-mirror">
+          <Button onClick={handleClick} variant="text">
+            아직 거울이 등록되지 않았습니다. <br /> 거울을 등록해주세요.
+          </Button>
+        </div>
+      }
     </>
   );
 }
