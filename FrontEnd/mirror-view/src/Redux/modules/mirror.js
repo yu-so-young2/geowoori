5f4c03633ca33bf829712921e97ac46346bf8da1
isabel-noh <nohhyunjeong93@gmail.com>
2023-02-15 09:52:09 +0900

    feat: websocket and teachablemachine binding

--- FrontEnd/mirror-view/src/Redux/modules/mirror.js
+++ FrontEnd/mirror-view/src/Redux/modules/mirror.js
@@ -5,13 +5,8 @@ const mirrorSlice = createSlice({
   initialState: {
     member: {
       memberKey: "fSBS-lCHb",
-<<<<<<< HEAD
-      nickname: "쏘영이",
-      birth: "1998-09-11",
-=======
       nickname: "짱윤이",
       birth: "1997-09-11",
->>>>>>> 3bf003754c8cd0a3c6309b6241af60b93c83696f
       kidsMode: true,
       widget: {
         news: true,
@@ -45,11 +40,8 @@ const mirrorSlice = createSlice({
     content: "",
     action: "",
     alertMsg: "",
-<<<<<<< HEAD
     level: "",
     point: "",
-=======
->>>>>>> 3bf003754c8cd0a3c6309b6241af60b93c83696f
   },
 
   reducers: {
@@ -85,13 +77,10 @@ const mirrorSlice = createSlice({
       state.action = "";
       state.image = "";
     },
-<<<<<<< HEAD
     getLev(state, action) {
       state.level = action.payload.lv;
       state.point = action.payload.exp;
     },
-=======
->>>>>>> 3bf003754c8cd0a3c6309b6241af60b93c83696f
   },
 });
 
