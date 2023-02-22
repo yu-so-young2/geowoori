71bec0c7611bda32fdb21589cd0b2ccdc6115faa
isabel-noh <nohhyunjeong93@gmail.com>
2023-02-22 12:13:20 +0900

    fixed : fortune teller fixed

--- FrontEnd/mirror-view/src/Components/FortuneTeller/FortuneTeller.jsx
+++ FrontEnd/mirror-view/src/Components/FortuneTeller/FortuneTeller.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const FortuneTeller = () => {
-    const fortune = useSelector((state) => state?.mirror?.member?.fortune);
+    const fortune = '이 고비를 잘 넘겨야 합니다.'
+    // const fortune = useSelector((state) => state?.mirror?.member?.fortune);
     return(
         <div className='fortune-teller'>
             <p className='fortune-p'>{fortune}</p>
